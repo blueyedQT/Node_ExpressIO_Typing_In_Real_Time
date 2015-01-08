0527aba19bec93a1b46ae7ef30b6ca50bd2c6843
Katrina Sanford <blueyed_qt@hotmail.com>
2015-01-07 20:39:39 -0800

    Saving progress, able to add users and save them

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,11 @@ module.exports = function Route(app){
 	app.get('/', function(req, res){
 		res.render('index', {title: 'Typing in Real Time' });
 	});
+	var users = [];
+	app.io.route('got_new_user', function(req){
+		app.io.broadcast('new_user', req.data.name);
+		users.push(req.data.name);
+		console.log(users);
+		req.io.emit('exisiting_users', users);
+	});
 }
