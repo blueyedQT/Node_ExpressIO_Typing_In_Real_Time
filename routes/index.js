ee34c1cdcdf1f88d753003585b9ca9d870d4f601
Katrina Sanford <blueyed_qt@hotmail.com>
2015-01-08 19:59:32 -0800

    Saving current work, in progress

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,28 @@ module.exports = function Route(app){
 		res.render('index', {title: 'Typing in Real Time' });
 	});
 	var users = [];
+	count=0;
 	app.io.route('got_new_user', function(req){
-		app.io.broadcast('new_user', req.data.name);
-		users.push(req.data.name);
-		console.log(users);
+		app.io.broadcast('new_user', {
+			name: req.data.name, count: count
+		});
+		users.push({name: req.data.name, count: count});
+		req.session.name = req.data.name;
+		req.session.count = count;
+		count++
 		req.io.emit('exisiting_users', users);
+	});
+	app.io.route('disconnect', function(req){
+		console.log(req);
+		for(var i=0; i<users.length; i++) {
+			var object = users[i];
+			if(object.id === req.session.count) {
+				users.splice(i, 1);
+				app.io.broadcast('user_disconnected', req.session.count);
+				console.log('disconnected', req.session.count);
+			}
+			console.log(req.session);
+		}
+		req.session.destroy();
 	});
 }
